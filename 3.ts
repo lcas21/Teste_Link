6d3c62fdd472e6eabcedc1b220bd8e14b934bb59
Lucas <lucassanciro@gmail.com>
2023-04-05 11:25:23 -0300

    eu não ví o arquivo no json no email

--- 3.ts
+++ 3.ts
@@ -1,65 +1,63 @@
-import * as fs from 'fs';
+import * as fs from "fs";
 
-const faturamentoTeste = 'faturamentoTeste.json';
-const ContfaturamentoTeste = fs.readFileSync(faturamentoTeste, 'utf-8');
+const faturamentoTeste = "dados.json";
+const ContfaturamentoTeste = fs.readFileSync(faturamentoTeste, "utf-8");
 
 const jsonObject = JSON.parse(ContfaturamentoTeste);
 
 console.log(jsonObject);
 
-function calcularMenorFaturamento(faturamento: number[]): number {
+function calcularMenorFaturamento(
+  dados: { dia: number; valor: number }[]
+): number {
+  let menorFaturamento = Infinity;
 
-    let menorFaturamento = faturamento[0];
-
-    for (let i = 1; i < faturamento.length; i++) {
-        const valor = faturamento[i];
-
-        if (valor > 0 && valor < menorFaturamento) {
-            menorFaturamento = valor;
-        }
+  for (let dado of dados) {
+    if (dado.valor > 0 && dado.valor < menorFaturamento) {
+      menorFaturamento = dado.valor;
     }
+  }
 
-    return menorFaturamento;
+  return menorFaturamento;
 }
 
-function calcularMaiorFaturamento(faturamento: number[]): number {
-
-    let maiorFaturamento = faturamento[0];
-
-    for (let i = 1; i < faturamento.length; i++) {
-        const valor = faturamento[i];
-
-        if (valor > 0 && valor > maiorFaturamento) {
-            maiorFaturamento = valor;
-        }
+function calcularMaiorFaturamento(
+    dados: { dia: number; valor: number }[]
+  ): number {
+    let maiorFaturamento = -Infinity;
+  
+    for (let dado of dados) {
+      if (dado.valor >  maiorFaturamento) {
+        maiorFaturamento = dado.valor;
+      }
     }
-
+  
     return maiorFaturamento;
-}
+  }
 
-function calcularMediaFaturamento(faturamento: number[]): number {
+function calcularMediaFaturamento(dados: { dia: number; valor: number }[]
+    ): number  {
+  let somaFaturamento = 0;
+  let diasComFaturamento = 0;
 
-    let somaFaturamento = 0;
-    let diasComFaturamento = 0;
-
-    for (let i = 1; i < faturamento.length; i++) {
-        const valor = faturamento[i];
-
-
-        if (valor > 0) {
-            somaFaturamento += valor;
-            diasComFaturamento++;
-        }
-
+  for (let dado of dados) {
+    if (dado.valor >  0) {
+      somaFaturamento += dado.valor;
+      diasComFaturamento++;
     }
+  }
 
-    const mediaFaturamento = somaFaturamento / diasComFaturamento;
+    if (diasComFaturamento === 0) {
+      return 0;
+  }
 
-    return mediaFaturamento;
+  const mediaFaturamento = somaFaturamento / diasComFaturamento;
+
+  return mediaFaturamento;
 }
 
 
-const faturamentoMensal = jsonObject.faturamento_Janeiro;
+const faturamentoMensal = jsonObject
 
 const menorFaturamentoDiario = calcularMenorFaturamento(faturamentoMensal);
 
@@ -67,24 +65,32 @@ const maiorFaturamentoDiario = calcularMaiorFaturamento(faturamentoMensal);
 
 const mediaFaturamentoDiario = calcularMediaFaturamento(faturamentoMensal);
 
-const diasMaiorMedia = calcularDiasMaiorMedia(faturamentoMensal);
 
-function calcularDiasMaiorMedia(faturamento: number[]): number {
-    let maiorQueMediaMensal = 0;
-    for (let i = 1; i < faturamento.length; i++) {
-        const valor = faturamento[i];
-        if (valor > mediaFaturamentoDiario) {
-            maiorQueMediaMensal++;
+
+function calcularDiasMaiorMedia(dados: {dia: number, valor: number}[]): number[] {
+        let maiorQueMediaMensal: number[] = [];
+    for (let dado of dados) {
+        if (dado.valor > mediaFaturamentoDiario) {
+            maiorQueMediaMensal.push(dado.dia);
         }
-    }
-    return maiorQueMediaMensal;
+       
+  
+}
+return maiorQueMediaMensal   
 }
 
+const diasMaiorMedia = calcularDiasMaiorMedia(faturamentoMensal);
 
-console.log(`O menor valor de faturamento diário é: R$ ${menorFaturamentoDiario}`);
+console.log(
+  `O menor valor de faturamento diário é: R$ ${menorFaturamentoDiario}`
+);
 
-console.log(`O maior valor de faturamento diário é: R$ ${maiorFaturamentoDiario}`);
+console.log(
+  `O maior valor de faturamento diário é: R$ ${maiorFaturamentoDiario}`
+);
 
 console.log(`A média de faturamento diário é: R$ ${mediaFaturamentoDiario}`);
 
-console.log(`O número de dias com faturamento maior que a média mensal é: ${diasMaiorMedia}`);
+
+
+console.log(`O número de dias com faturamento maior que a média é: ${diasMaiorMedia}`)   
